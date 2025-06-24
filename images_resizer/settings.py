b50a0bde16a99224f02a32855cbbf1145f93ffcd
rishavp15 <rishavprakash14@gmail.com>
2025-06-24 14:30:35 +0530

    Fix: add .gitignore, remove db.sqlite3 from repo, update Procfile for collectstatic, and clean up settings.py

--- images_resizer/settings.py
+++ images_resizer/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
@@ -173,6 +173,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# 404 handler
 handler404 = 'django.views.defaults.page_not_found'
 
 if DEBUG:
